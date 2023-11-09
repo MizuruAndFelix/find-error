a8c7cb04d93681d091023cbfdda1943a9e1eed86
Misuiro <113217149+MizuruAndFelix@users.noreply.github.com>
2023-11-09 18:15:28 +0300

    1

---
+++ fined error/fined error.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+struct point {
+	double m_x = 0.0;
+	double m_y = 0.0;
+
+	point(double x, double y)
+	{
+		m_x = x;
+		m_y = y;
+	}
+};
+
+void print_point(const point& point_object)
+{
+	std::cout << "x:" << point_object.m_x << ", y: " << point_object.m_y << std::endl;
+}
+
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		point myPoint(i, 2 * i);
+		print_point(myPoint);
+	}
+	return 0;
+}
